b1eeefad26c0dacfa1d8d5fa2fa97534ea2fe5bf
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-05 07:59:16 -0300

    Artículo nuevo

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  // { href: '/tags', title: 'Tags' },
+  { href: '/tags', title: 'Tags' },
   { href: '/proyectos', title: 'Proyectos' },
   { href: '/acerca', title: 'Acerca de' },
 ]
