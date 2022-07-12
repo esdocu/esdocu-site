9ddd4e656477a47026995d549ac35ac7e2f0aa98
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-12 17:00:35 -0300

    Formulario Newsletter

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -1,26 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import mailchimp from '@mailchimp/mailchimp_marketing'
-import Cors from 'cors'
-
-// Initializing the cors middleware
-// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Cors) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
+import NextCors from 'nextjs-cors'
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
@@ -29,13 +9,19 @@ mailchimp.setConfig({
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // Run the middleware
-  await runMiddleware(req, res, cors)
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
 
   const { email } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'El correo electrónico es requerido 4' })
+    return res.status(400).json({ error: 'El correo electrónico es requerido 5' })
   }
 
   try {
