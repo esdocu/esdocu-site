d6b9b8088c10d542df160d89871ad8991b91fa6e
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-12 12:11:32 -0300

    Se añadió el formulario de Newsletters

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -1,18 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import mailchimp from '@mailchimp/mailchimp_marketing'
 
-// TEMP
-const _process = {
-  env: {
-    MAILCHIMP_API_KEY: '91f2c57a906143af13b456b9029766c6-us14',
-    MAILCHIMP_API_SERVER: 'us14',
-    MAILCHIMP_AUDIENCE_ID: '1c62d0fe2c',
-  },
-}
-
 mailchimp.setConfig({
-  apiKey: _process.env.MAILCHIMP_API_KEY,
-  server: _process.env.MAILCHIMP_API_SERVER, // E.g. us1
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER, // E.g. us1
 })
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -24,7 +15,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   try {
-    await mailchimp.lists.addListMember(_process.env.MAILCHIMP_AUDIENCE_ID, {
+    await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
       email_address: email,
       status: 'subscribed',
     })
