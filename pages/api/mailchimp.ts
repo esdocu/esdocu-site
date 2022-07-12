d4c5364c670de46d4ebf2859b2a80c62b48c755f
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-12 16:37:34 -0300

    Formulario Newsletter

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -1,5 +1,35 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import mailchimp from '@mailchimp/mailchimp_marketing'
+import Cors from 'cors'
+
+// const whitelist = ['http://localhost:3000', 'https://getbootstrap.esdocu.com']
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'POST'],
+  // origin: function (origin, callback) {
+  //   console.log(origin)
+  //   if (whitelist.indexOf(origin) !== -1) {
+  //     callback(null, true)
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'))
+  //   }
+  // },
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Cors) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
@@ -8,10 +38,13 @@ mailchimp.setConfig({
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
   const { email } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'El correo electrónico es requerido' })
+    return res.status(400).json({ error: 'El correo electrónico es requerido 3' })
   }
 
   try {
