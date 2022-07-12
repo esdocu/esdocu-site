e5e9febe0912e5081df8b645df897679683ea5fb
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-12 17:10:46 -0300

    Formulario Newsletter

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -21,7 +21,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'El correo electrónico es requerido 5' })
+    return res.status(400).json({ error: 'El correo electrónico es requerido 6' })
   }
 
   try {
