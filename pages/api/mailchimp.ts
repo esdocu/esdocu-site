af04094db3676741f4789b867493d000311acba2
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-12 13:46:23 -0300

    Se añadió el formulario de Newsletters

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -11,7 +11,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'El correo electrónico es requerido' })
+    return res
+      .status(400)
+      .json({ error: 'El correo electrónico es requerido 2 ' + process.env.MAILCHIMP_API_SERVER })
   }
 
   try {
