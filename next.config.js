a3f40b692586d219e3f47e8683c8444c499f7d11
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-13 09:08:34 -0300

    Formulario Newsletter

--- next.config.js
+++ next.config.js
@@ -70,25 +70,25 @@ module.exports = withContentlayer(
         //   source: '/(.*)',
         //   headers: securityHeaders,
         // },
-        // {
-        //   // matching all API routes
-        //   source: '/api/:path*',
-        //   headers: [
-        //     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-        //     {
-        //       key: 'Referrer-Policy',
-        //       value: 'unsafe-url',
-        //     },
-        //     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        //     { key: 'Access-Control-Allow-Origin', value: 'https://getbootstrap.esdocu.com' },
-        //     { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-        //     {
-        //       key: 'Access-Control-Allow-Headers',
-        //       value:
-        //         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-        //     },
-        //   ],
-        // },
+        {
+          // matching all API routes
+          source: '/api/(.*)',
+          headers: [
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+            {
+              key: 'Referrer-Policy',
+              value: 'unsafe-url',
+            },
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+          ],
+        },
       ]
     },
     webpack: (config, { dev, isServer }) => {
