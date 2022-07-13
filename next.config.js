f09521de40654f2ed0d68d554b519ba93dc9b11a
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-13 10:01:14 -0300

    Formulario Newsletter

--- next.config.js
+++ next.config.js
@@ -64,29 +64,11 @@ module.exports = withContentlayer(
     eslint: {
       dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
     },
-    poweredByHeader: false,
-    generateEtags: false,
-    httpAgentOptions: {
-      keepAlive: false,
-    },
     async headers() {
       return [
         {
           source: '/(.*)',
           headers: securityHeaders,
-        },
-        {
-          source: '/api/mailchimp',
-          headers: [
-            { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-            {
-              key: 'Access-Control-Allow-Headers',
-              value:
-                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            },
-          ],
         },
       ]
     },
