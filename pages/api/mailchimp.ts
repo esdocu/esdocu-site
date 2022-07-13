88941ca74d8319a6b9d78f528cd86cde2f820630
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-13 09:39:55 -0300

    Formulario Newsletter

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -1,13 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import mailchimp from '@mailchimp/mailchimp_marketing'
 // import NextCors from 'nextjs-cors'
-import Cors from 'cors'
+// import Cors from 'cors'
 
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
-})
+// const cors = Cors({
+//   methods: ['POST', 'GET', 'HEAD'],
+// })
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
@@ -16,22 +16,22 @@ mailchimp.setConfig({
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+// function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
 
-      return resolve(result)
-    })
-  })
-}
+//       return resolve(result)
+//     })
+//   })
+// }
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Run the middleware
-  await runMiddleware(req, res, cors)
+  // await runMiddleware(req, res, cors)
 
   // Run the cors middleware
   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
@@ -46,7 +46,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'El correo electrónico es requerido 1' })
+    return res.status(400).json({ error: 'El correo electrónico es requerido 2' })
   }
 
   try {
