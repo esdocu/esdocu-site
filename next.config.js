c34d6327db8ffdfda9381b9362562ccb74603c86
Fabian Karaben <fabiankaraben@gmail.com>
2022-07-13 08:39:38 -0300

    Formulario Newsletter

--- next.config.js
+++ next.config.js
@@ -80,7 +80,7 @@ module.exports = withContentlayer(
               value: 'unsafe-url',
             },
             { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Origin', value: 'https://getbootstrap.esdocu.com' },
             { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
             {
               key: 'Access-Control-Allow-Headers',
--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -21,7 +21,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
 
   if (!email) {
-    return res.status(400).json({ error: 'El correo electrónico es requerido 3' })
+    return res.status(400).json({ error: 'El correo electrónico es requerido 4' })
   }
 
   try {
