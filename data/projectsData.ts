7924a27cc199d1e315e16a255122a257607f01df
Fabian Karaben <fabiankaraben@gmail.com>
2022-09-01 12:35:45 -0300

    Añade el proyecto 'Dart en Español'

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,13 +6,27 @@ const projectsData = [
     href: 'https://getbootstrap.esdocu.com/',
     done: true,
   },
+  // {
+  //   title: 'Tailwind CSS en Español (traduciendo)',
+  //   description: `Crea rápidamente sitios web modernos sin tener que abandonar tu HTML. Un framework CSS repleto de clases que se pueden componer para crear cualquier diseño, directamente en tu marcado.`,
+  //   imgSrc: '/static/images/projects/tailwindcss.jpg',
+  //   href: 'https://github.com/esdocu/tailwindcss',
+  //   done: false,
+  // },
   {
-    title: 'Tailwind CSS en Español (traduciendo)',
-    description: `Crea rápidamente sitios web modernos sin tener que abandonar tu HTML. Un framework CSS repleto de clases que se pueden componer para crear cualquier diseño, directamente en tu marcado.`,
-    imgSrc: '/static/images/projects/tailwindcss.jpg',
-    href: 'https://github.com/esdocu/tailwindcss',
-    done: false,
+    title: 'Dart en Español',
+    description: `Dart es un lenguaje optimizado para el cliente para aplicaciones rápidas en cualquier plataforma.`,
+    imgSrc: '/static/images/projects/dart.jpg',
+    href: 'https://dart.esdocu.com',
+    done: true,
   },
+  // {
+  //   title: 'Flutter en Español',
+  //   description: `Flutter transforma el proceso de desarrollo de aplicaciones. Crea, prueba e implementa hermosas aplicaciones móviles, web, de escritorio e integradas desde una única base de código.`,
+  //   imgSrc: '/static/images/projects/flutter.jpg',
+  //   href: 'https://flutter.esdocu.com',
+  //   done: true,
+  // },
 ]
 
 export default projectsData
