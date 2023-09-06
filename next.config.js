70720ad3d39d12e036b165cee67d7aa8d2232622
Fabián Karaben <fabiankaraben@gmail.com>
2023-09-06 20:37:46 -0300

    Adds a new but still imcoomplete home design

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  latex: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true,
 })
 
 module.exports = withNextra({
+  reactStrictMode: true,
+  eslint: {
+    // Eslint behaves weirdly in this monorepo.
+    ignoreDuringBuilds: true
+  },
   webpack(config) {
     const allowedSvgRegex = /components\/icons\/.+\.svg$/
 
@@ -16,6 +26,7 @@ module.exports = withNextra({
       test: allowedSvgRegex,
       use: ['@svgr/webpack']
     })
+
     return config
   }
 })
